fc9f989719565564ba5ab2e59ed81cd4872aaccc
Tim Prescott <teepeemm@gmail.com>
2026-05-09 20:38:18 -0500

    some over to frac

--- text/quick.py
+++ text/quick.py
@@ -1,15 +1,11 @@
 
+import re
 from glob import glob
 
 for texfile in glob('*.tex'):
   with open(texfile) as tex:
     mathopen = False
     content = tex.read()
-  while '$$' in content:
-    if mathopen:
-      content = content.replace('$$',r'\]',1)
-    else:
-      content = content.replace('$$',r'\[',1)
-    mathopen = not mathopen
+  content = re.sub(r'{(\d+)\\over(\d+)}',r'\\frac{\1}{\2}',content)
   with open(texfile,'w') as tex:
     tex.write(content)
