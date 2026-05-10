51a41eed962018f251fd1c6858027ad35c166f0f
Tim Prescott <teepeemm@gmail.com>
2026-05-09 21:07:03 -0500

    more over to frac

--- text/quick.py
+++ text/quick.py
@@ -6,6 +6,6 @@ for texfile in glob('*.tex'):
   with open(texfile) as tex:
     mathopen = False
     content = tex.read()
-  content = re.sub(r'{(\d+)\\over(\d+)}',r'\\frac{\1}{\2}',content)
+  content = re.sub(r'{(\d+)\\over\s*(\d+)}',r'\\frac{\1}{\2}',content)
   with open(texfile,'w') as tex:
     tex.write(content)
