ba0ffa9b19db5b396f328b39101532e8f2ef53a9
Tim Prescott <teepeemm@gmail.com>
2026-05-11 10:24:50 -0500

    more over to frac

--- text/quick.py
+++ text/quick.py
@@ -7,9 +7,16 @@ for texfile in glob('*.tex'):
     mathopen = False
     content = tex.read()
   # simplegroup = r'([ a-zA-Z0-9^_\\+-]+)'  # No parentheses or grouping
-  content = re.sub(r'{([ a-zA-Z0-9^_\\+-]+)\\over(\s*|(?=\W))',r'\\frac{\1}{',content)
-  content = re.sub(r'\\left\(([ a-zA-Z0-9^_\\+-]+)\\over\s*([ a-zA-Z0-9^_\\+-]+)\\right\)',
-                    r'\\frac{\1}{\2}',content)
+  content = re.sub(r'''{
+                    ([ a-zA-Z0-9^_\\+-]+)  # a simple group (no parentheses or grouping)
+                    \\over(\s*|(?=\W))''',  # watch out for \overline
+                    r'\\frac{\1}{', content, flags=re.VERBOSE)
+  content = re.sub(r'''(?<=\\left[({[|])  # left ({[|
+                    ([ a-zA-Z0-9^_\\+-]+)  # a simple group
+                    \\over\s*
+                    ([ a-zA-Z0-9^_\\+-]+)  # a simple group
+                    (?=\\right[)}\]|])''',  # right )}]|
+                    r'\\frac{\1}{\2}', content, flags=re.VERBOSE)
 #  content = re.sub(r'{(\d+)\\over\s*(\d+)}',r'\\frac{\1}{\2}',content)
   with open(texfile,'w') as tex:
     tex.write(content)
