c0ac4de45b571be6d547ac49df85ab6de3a27f37
Tim Prescott <teepeemm@gmail.com>
2026-05-11 09:27:45 -0500

    more over to frac

--- text/quick.py
+++ text/quick.py
@@ -6,7 +6,7 @@ for texfile in glob('*.tex'):
   with open(texfile) as tex:
     mathopen = False
     content = tex.read()
-  content = re.sub(r'{([a-zA-Z0-9^_\\]+)\\over(\s*|(?=\W))',r'\\frac{\1}{',content)
+  content = re.sub(r'{([a-zA-Z0-9^_\\+-]+)\\over(\s*|(?=\W))',r'\\frac{\1}{',content)
 #  content = re.sub(r'{(\d+)\\over\s*(\d+)}',r'\\frac{\1}{\2}',content)
   with open(texfile,'w') as tex:
     tex.write(content)
