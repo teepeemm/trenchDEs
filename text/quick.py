87cb1db2aa670687ad8142f57697855fc47d9814
Tim Prescott <teepeemm@gmail.com>
2026-05-11 10:46:59 -0500

    more over to frac

--- text/quick.py
+++ text/quick.py
@@ -8,13 +8,13 @@ for texfile in glob('*.tex'):
     content = tex.read()
   # simplegroup = r'([ a-zA-Z0-9^_\\+-]+)'  # No parentheses or grouping
   content = re.sub(r'''{
-                    ([ a-zA-Z0-9^_\\+!-]+)  # a simple group (no parentheses or grouping)
+                    ([ a-zA-Z0-9^_\\+!.-]+)  # a simple group (no parentheses or grouping)
                     \\over(\s*|(?=\W))''',  # watch out for \overline
                     r'\\frac{\1}{', content, flags=re.VERBOSE)
   content = re.sub(r'''(?<=\\left[({[|])  # left ({[|
-                    ([ a-zA-Z0-9^_\\+!-]+)  # a simple group
+                    ([ a-zA-Z0-9^_\\+!.-]+)  # a simple group
                     \\over\s*
-                    ([ a-zA-Z0-9^_\\+!-]+)  # a simple group
+                    ([ a-zA-Z0-9^_\\+!.-]+)  # a simple group
                     (?=\\right[)}\]|])''',  # right )}]|
                     r'\\frac{\1}{\2}', content, flags=re.VERBOSE)
 #  content = re.sub(r'{(\d+)\\over\s*(\d+)}',r'\\frac{\1}{\2}',content)
