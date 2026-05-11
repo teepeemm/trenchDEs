2e89fbbf6936f27c7a994a65a83a63d235510096
Tim Prescott <teepeemm@gmail.com>
2026-05-11 11:13:52 -0500

    save over to frac python

--- text/quick.py
+++ text/quick.py
@@ -6,17 +6,16 @@ for texfile in glob('*.tex'):
   with open(texfile) as tex:
     mathopen = False
     content = tex.read()
-  # simplegroup = r'([ a-zA-Z0-9^_\\+-]+)'  # No parentheses or grouping
   content = re.sub(r'''{
                     ([ a-zA-Z0-9^_\\+!.'-]+)  # a simple group (no parentheses or grouping)
                     \\over(\s*|(?=\W))''',  # watch out for \overline
                     r'\\frac{\1}{', content, flags=re.VERBOSE)
+  # but \left(...\right) forms its own group
   content = re.sub(r'''(?<=\\left[({[|])  # left ({[|
                     ([ a-zA-Z0-9^_\\+!.'-]+)  # a simple group
                     \\over\s*
                     ([ a-zA-Z0-9^_\\+!.'-]+)  # a simple group
                     (?=\\right[)}\]|])''',  # right )}]|
                     r'\\frac{\1}{\2}', content, flags=re.VERBOSE)
-#  content = re.sub(r'{(\d+)\\over\s*(\d+)}',r'\\frac{\1}{\2}',content)
   with open(texfile,'w') as tex:
     tex.write(content)
