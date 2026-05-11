787334fc8a090747382164b874fbf6c5900f4313
Tim Prescott <teepeemm@gmail.com>
2026-05-11 09:56:01 -0500

    almost over to frac

--- text/quick.py
+++ text/quick.py
@@ -6,7 +6,10 @@ for texfile in glob('*.tex'):
   with open(texfile) as tex:
     mathopen = False
     content = tex.read()
+  # simplegroup = r'([ a-zA-Z0-9^_\\+-]+)'  # No parentheses or grouping
   content = re.sub(r'{([ a-zA-Z0-9^_\\+-]+)\\over(\s*|(?=\W))',r'\\frac{\1}{',content)
+  content = re.sub(r'\\left\(([ a-zA-Z0-9^_\\+-]+)\\over\s*([ a-zA-Z0-9^_\\+-]+)\\right\)',
+                    r'\\frac{\1}{\2}',content)
 #  content = re.sub(r'{(\d+)\\over\s*(\d+)}',r'\\frac{\1}{\2}',content)
   with open(texfile,'w') as tex:
     tex.write(content)
