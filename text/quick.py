624691039dad15eeddb82cd2c2ca59a9633fc2e4
Tim Prescott <teepeemm@gmail.com>
2026-05-13 14:47:03 -0500

    update

--- text/quick.py
+++ text/quick.py
@@ -2,10 +2,7 @@
 import re
 from glob import glob
 
-for texfile in glob('*.tex'):
-  with open(texfile) as tex:
-    mathopen = False
-    content = tex.read()
+def replace_over(content: str) -> str:
   content = re.sub(r'''{
                     ([ a-zA-Z0-9^_\\+!.'-]+)  # a simple group (no parentheses or grouping)
                     \\over(\s*|(?=\W))''',  # watch out for \overline
@@ -17,5 +14,17 @@ for texfile in glob('*.tex'):
                     ([ a-zA-Z0-9^_\\+!.'-]+)  # a simple group
                     (?=\\right[)}\]|])''',  # right )}]|
                     r'\\frac{\1}{\2}', content, flags=re.VERBOSE)
+  return content
+
+def trim_trailing_space(content: str) -> str:
+  content = re.sub(r'(?<=\\item\\label{exer:\d+.\d+.\d+})\s*$', '%', content)
+  content = re.sub(r'(?<=\\item\\label{exer:\d+.\d+.\d+})\s+(?=\S)', '', content)
+  return content
+
+for texfile in glob('*.tex'):
+  with open(texfile) as tex:
+    mathopen = False
+    content = tex.read()
+  content = trim_trailing_space(content)
   with open(texfile,'w') as tex:
     tex.write(content)
